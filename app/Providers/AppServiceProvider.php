e30903ef263a085b0df43d16ea236d80f5ab1514
Claudson Martins <claudson@outlook.com>
2020-10-24 16:06:29 -0300

    Faker em portugues

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->singleton(
+            \Faker\Generator::class,
+            fn () => \Faker\Factory::create('pt_BR')
+        );
     }
 
     /**

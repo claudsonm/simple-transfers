07daf24d94dadc0c3dbb76b016ffb7363047d22f
Claudson Martins <claudson@outlook.com>
2020-10-25 11:43:24 -0300

    Adicionados casos de erro

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,12 +2,55 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
     use RefreshDatabase;
+
+    /** @test */
+    public function it_cant_create_a_user_with_an_existing_email()
+    {
+        $frank = User::factory()->create(['email' => 'frank@sinatra.com']);
+        $data = [
+            'name' => 'Tom Jobim',
+            'email' => $frank->email,
+            'password' => '!123456Tom',
+            'password_confirmation' => '!123456Tom',
+            'document_number' => '89862935014',
+        ];
+
+        $this->postJson('api/users', $data)
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'email' => 'The email has already been taken.'
+            ]);
+
+        $this->assertDatabaseMissing('users', ['id' => 2]);
+    }
+
+    /** @test */
+    public function it_cant_create_a_user_with_an_existing_document_number()
+    {
+        $marie = User::factory()->create();
+        $data = [
+            'name' => 'Tom Jobim',
+            'email' => 'tom@bossa.com',
+            'password' => '!123456Tom',
+            'password_confirmation' => '!123456Tom',
+            'document_number' => $marie->document_number,
+        ];
+
+        $this->postJson('api/users', $data)
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([
+                'document_number' => 'The document number has already been taken.'
+            ]);
+
+        $this->assertDatabaseMissing('users', ['id' => 2]);
+    }
 
     /** @test */
     public function it_can_create_a_physical_person_user()
@@ -19,6 +62,7 @@ class UserTest extends TestCase
             'password_confirmation' => '!123456Tom',
             'document_number' => '89862935014',
         ];
+
         $this->postJson('api/users', $data)
             ->assertCreated()
             ->assertJsonPath('id', 1)
@@ -42,6 +86,7 @@ class UserTest extends TestCase
             'password_confirmation' => '!123456Evil',
             'document_number' => '04461907000109',
         ];
+
         $this->postJson('api/users', $data)
             ->assertCreated()
             ->assertJsonPath('id', 1)
